ccb65b819eaa37e2ea6afcc3aa277b19d29ac994
jai singh verma <jaisingh051297@gmail.com>
2023-04-04 17:49:26 +0530

    adding cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express=require('express');
+const cors=require('cors')
 require('./db/mongoose');
 const userRouter=require('./routers/user');
 const taskRouter=require('./routers/task');
 
 const app=express();
+app.use(cors())
 const port=process.env.PORT
 app.use(express.json())
 
